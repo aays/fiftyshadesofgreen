730d7f253a8f530b6f9bbc452314133b59557a3f
Ahmed Hasan <ahmed.hasan@mail.utoronto.ca>
2017-05-16 12:29:28 -0400

    Update and rename r2filemaker.R to r2corrector.R

--- r2corrector.R
+++ r2corrector.R
@@ -1,8 +1,11 @@
 #!usr/bin/env Rscript
 
 # generates csv files out of plink table-formatted r2 output to then be used as input for zerocorrector.py
-# usage - Rscript r2filemaker.R [filename]
-# reference - notebook 8.7
+
+# usage - Rscript r2corrector.R [filename]
+# eg r2corrector.R chromsome1.ld will generate a file called chromosome1.ldz in the same dir
+
+# AH - 04/2017
 
 library(tidyr)
 library(dplyr)
@@ -28,6 +31,6 @@ separate(col = "column", into = c('chr1', 'pos1', '.', 'chr2', 'pos2', '.', 'r2'
 
 chrdf <- remover(data)
 
-outname <- paste(infile, '2', sep = '')
+outname <- paste(infile, 'z', sep = '')
 
 write.csv(chrdf, file = outname) 
