086a0849f03caa9577ffca1cf75f0a8b3d617439
Ahmed Hasan <ahmed.hasan@mail.utoronto.ca>
2017-06-20 15:30:58 -0400

    now *actually* ensures four haplotypes present

--- vcf/ldcalc.py
+++ vcf/ldcalc.py
@@ -96,7 +96,8 @@ def singlevcfcalc(vcf_file, ref, target, stat):
         if len(record1.ALT) > 1:
             continue
         for record2 in targetlocus:
-            if len(freqsgetter(record1, record2)[2]) < 4: # ensure four haps present
+            haps = freqsgetter(record1, record2)[2]
+            if 0 in haps.values(): # ensure four haps present
                 continue
             if len(record2.ALT) > 1:
                 continue
