2a1278b1f0753c15570d5faf0e448d66ef5e930c
Ahmed Hasan <ahmed.hasan@mail.utoronto.ca>
2017-07-30 19:59:30 -0400

    Create slidingmeans.R

---
+++ ldhelmet/slidingmeans.R
@@ -0,0 +1,29 @@
+# does what slidingmeans.py does, but in R.
+# takes in LDhelmet output and a windowsize
+# ie. Rscript slidingmeans.R [ldhelmet file] [windowsize]
+# saves to same directory (for now)
+
+# AH - 07/2017
+
+library(dplyr, warn.conflicts = FALSE)
+library(magrittr, warn.conflicts = FALSE)
+
+args <- commandArgs(trailingOnly = TRUE)
+
+file <- args[1]
+windowsize <- args[2]
+
+windowcalc <- function(df, windowsize) {
+    df %<>% select(left_snp = V1, right_snp = V2, rho = V3) %>% # get cols
+        mutate(diff = right_snp - left_snp) %>% # get diff
+        mutate(scaledrho = rho * diff) %>% # use diff for scaled rho
+        mutate(window = ceiling(left_snp/windowsize)) %>% # compute windows
+        group_by(window) %>% # group by windows
+        summarise(rho = mean(scaledrho)) # get means
+    return(df)
+}
+
+df <- read.csv(file, sep = ' ', skip = 3, header = FALSE) %>%
+  windowcalc(windowsize)
+
+write.table(df, file = paste('means_', infile, sep = ''), sep = ' ')  
