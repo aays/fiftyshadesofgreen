2c20272889a4dbbea19a174f09307ad2364d7b97
Ahmed Hasan <ahmed.hasan@mail.utoronto.ca>
2017-07-31 12:49:32 -0400

    custom outfile name

--- ldhelmet/slidingmeans.R
+++ ldhelmet/slidingmeans.R
@@ -1,8 +1,7 @@
 # does what slidingmeans.py does, but in R.
 # takes in LDhelmet output and a windowsize
 # ie. Rscript slidingmeans.R [ldhelmet file] [windowsize] [chromosome name]
-# saves to 'genomedist.txt' in same directory (for now)
-# for multiple chromosomes - for i in {1..n}; do Rscript slidingmeans.R chromosome_$i.txt 200000 chromosome_$i; done
+# for multiple chromosomes - for i in {1..n}; do Rscript slidingmeans.R chromosome_$i.txt 200000 chromosome_$i genome.txt; done
 
 # AH - 07/2017
 
@@ -14,6 +13,7 @@ args <- commandArgs(trailingOnly = TRUE)
 filename <- as.character(args[1])
 window <- as.integer(args[2])
 chrname <- as.character(args[3])
+outname <- as.character(args[4])
 
 windowcalc <- function(df, windowsize) {
     df %<>% select(left_snp = V1, right_snp = V2, rho = V3) %>% # get cols
@@ -33,5 +33,5 @@ df %<>% windowcalc(window)
 df <- df[,c(2,1)] # reorder
 
 names(df) <- NULL # suppress colnames in printing
-write.table(df, file = 'genomedist.txt', sep = ' ', 
+write.table(df, file = outname, sep = ' ', 
             col.names = FALSE, quote = FALSE, append = TRUE)
