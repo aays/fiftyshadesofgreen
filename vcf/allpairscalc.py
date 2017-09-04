98d177bfb10360b7a5606d7d6911423d66a70731
Ahmed Hasan <ahmed.hasan@mail.utoronto.ca>
2017-09-04 18:09:07 -0400

    Update allpairscalc.py

--- vcf/allpairscalc.py
+++ vcf/allpairscalc.py
@@ -28,47 +28,45 @@ vcfin = args.vcfinput
 ldstats = args.ldstats
 haps = args.haps
 
-def ldgetter(record1, record2, stat):
-    '''Helper fxn for allpairscalc
-    '''
-    
-    if haps == False: # proceed w/o haps
-        if len(stat) == 1:
-            if 'd' in stat:
-                print(metadata(record1, record2), dcalc(record1, record2))
-            elif 'dprime' in stat:
-                print(metadata(record1, record2), dprimecalc(record1, record2))
-            elif 'r2' in stat:
-                print(metadata(record1, record2), r2calc(record1, record2))
-        elif len(stat) == 2:
-            if 'd' in stat and 'dprime' in stat:
-                print(metadata(record1, record2), dcalc(record1, record2), dprimecalc(record1, record2))
-            elif 'd' in stat and 'r2' in stat:
-                print(metadata(record1, record2), dcalc(record1, record2), r2calc(record1, record2))
-            elif 'dprime' in stat and 'r2' in stat:
-                print(metadata(record1, record2), dcalc(record1, record2), r2calc(record1, record2))
-        elif len(stat) == 3:
-            print(metadata(record1, record2), dcalc(record1, record2), dprimecalc(record1, record2), r2calc(record1, record2))
-    
-    elif haps == True: # show haps/4 for each comparison
-        observed_haps = list(freqsgetter(record1, record2)[2].values()) # get hap frequencies
-        hapcount = 4 - observed_haps.count(0)
-        if len(stat) == 1:
-            if 'd' in stat:
-                print(metadata(record1, record2), dcalc(record1, record2), hapcount)
-            elif 'dprime' in stat:
-                print(metadata(record1, record2), dprimecalc(record1, record2), hapcount)
-            elif 'r2' in stat:
-                print(metadata(record1, record2), r2calc(record1, record2), hapcount)
-        elif len(stat) == 2:
-            if 'd' in stat and 'dprime' in stat:
-                print(metadata(record1, record2), dcalc(record1, record2), dprimecalc(record1, record2), hapcount)
-            elif 'd' in stat and 'r2' in stat:
-                print(metadata(record1, record2), dcalc(record1, record2), r2calc(record1, record2), hapcount)
-            elif 'dprime' in stat and 'r2' in stat:
-                print(metadata(record1, record2), dcalc(record1, record2), r2calc(record1, record2), hapcount)
-        elif len(stat) == 3:
-            print(metadata(record1, record2), dcalc(record1, record2), dprimecalc(record1, record2), r2calc(record1, record2), hapcount)
+  def ldgetter(record1, record2, stat):
+
+      if haps == False: # proceed w/o haps
+          if len(stat) == 1:
+              if 'd' in stat:
+                  print(metadata(record1, record2), dcalc(record1, record2))
+              elif 'dprime' in stat:
+                  print(metadata(record1, record2), dprimecalc(record1, record2))
+              elif 'r2' in stat:
+                  print(metadata(record1, record2), r2calc(record1, record2))
+          elif len(stat) == 2:
+              if 'd' in stat and 'dprime' in stat:
+                  print(metadata(record1, record2), dcalc(record1, record2), dprimecalc(record1, record2))
+              elif 'd' in stat and 'r2' in stat:
+                  print(metadata(record1, record2), dcalc(record1, record2), r2calc(record1, record2))
+              elif 'dprime' in stat and 'r2' in stat:
+                  print(metadata(record1, record2), dcalc(record1, record2), r2calc(record1, record2))
+          elif len(stat) == 3:
+              print(metadata(record1, record2), dcalc(record1, record2), dprimecalc(record1, record2), r2calc(record1, record2))
+
+      elif haps == True: # show haps/4 for each comparison
+          observed_haps = list(freqsgetter(record1, record2)[2].values()) # get hap frequencies
+          hapcount = 4 - observed_haps.count(0)
+          if len(stat) == 1:
+              if 'd' in stat:
+                  print(metadata(record1, record2), dcalc(record1, record2), hapcount)
+              elif 'dprime' in stat:
+                  print(metadata(record1, record2), dprimecalc(record1, record2), hapcount)
+              elif 'r2' in stat:
+                  print(metadata(record1, record2), r2calc(record1, record2), hapcount)
+          elif len(stat) == 2:
+              if 'd' in stat and 'dprime' in stat:
+                  print(metadata(record1, record2), dcalc(record1, record2), dprimecalc(record1, record2), hapcount)
+              elif 'd' in stat and 'r2' in stat:
+                  print(metadata(record1, record2), dcalc(record1, record2), r2calc(record1, record2), hapcount)
+              elif 'dprime' in stat and 'r2' in stat:
+                  print(metadata(record1, record2), dcalc(record1, record2), r2calc(record1, record2), hapcount)
+          elif len(stat) == 3:
+              print(metadata(record1, record2), dcalc(record1, record2), dprimecalc(record1, record2), r2calc(record1, record2), hapcount)
 
 def allpairscalc(vcf_file, stat, haps = False):
     '''In a single VCF, calculates linkage stats between all possible pairs, regardless of region.
@@ -83,6 +81,45 @@ def allpairscalc(vcf_file, stat, haps = False):
     def metadata(record1, record2):
         out = record1.CHROM + ' ' + str(record1.POS) + ' ' + record2.CHROM + ' ' + str(record2.POS)
         return out
+      
+    def ldgetter(record1, record2):
+        if haps == False: # proceed w/o haps
+            if len(stat) == 1:
+                if 'd' in stat:
+                    print(metadata(record1, record2), dcalc(record1, record2))
+                elif 'dprime' in stat:
+                    print(metadata(record1, record2), dprimecalc(record1, record2))
+                elif 'r2' in stat:
+                    print(metadata(record1, record2), r2calc(record1, record2))
+            elif len(stat) == 2:
+                if 'd' in stat and 'dprime' in stat:
+                    print(metadata(record1, record2), dcalc(record1, record2), dprimecalc(record1, record2))
+                elif 'd' in stat and 'r2' in stat:
+                    print(metadata(record1, record2), dcalc(record1, record2), r2calc(record1, record2))
+                elif 'dprime' in stat and 'r2' in stat:
+                    print(metadata(record1, record2), dcalc(record1, record2), r2calc(record1, record2))
+            elif len(stat) == 3:
+                print(metadata(record1, record2), dcalc(record1, record2), dprimecalc(record1, record2), r2calc(record1, record2))
+
+        elif haps == True: # show haps/4 for each comparison
+            observed_haps = list(freqsgetter(record1, record2)[2].values()) # get hap frequencies
+            hapcount = 4 - observed_haps.count(0)
+            if len(stat) == 1:
+                if 'd' in stat:
+                    print(metadata(record1, record2), dcalc(record1, record2), hapcount)
+                elif 'dprime' in stat:
+                    print(metadata(record1, record2), dprimecalc(record1, record2), hapcount)
+                elif 'r2' in stat:
+                    print(metadata(record1, record2), r2calc(record1, record2), hapcount)
+            elif len(stat) == 2:
+                if 'd' in stat and 'dprime' in stat:
+                    print(metadata(record1, record2), dcalc(record1, record2), dprimecalc(record1, record2), hapcount)
+                elif 'd' in stat and 'r2' in stat:
+                    print(metadata(record1, record2), dcalc(record1, record2), r2calc(record1, record2), hapcount)
+                elif 'dprime' in stat and 'r2' in stat:
+                    print(metadata(record1, record2), dcalc(record1, record2), r2calc(record1, record2), hapcount)
+            elif len(stat) == 3:
+                print(metadata(record1, record2), dcalc(record1, record2), dprimecalc(record1, record2), r2calc(record1, record2), hapcount) 
 
     left = snppuller(vcf_file) # create ref vcf record generator
     stat = stat.split('/') # get stat
@@ -96,7 +133,7 @@ def allpairscalc(vcf_file, stat, haps = False):
             if record1.CHROM == record2.CHROM and record1.POS == record2.POS:
                 continue
             else:
-                ldgetter(record1, record2, stat)
+                ldgetter(record1, record2)
 
 allpairscalc(vcf_file = vcfin, stat = ldstats, haps = haps)
 
