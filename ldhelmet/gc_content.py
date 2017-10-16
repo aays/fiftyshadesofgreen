53e478bf39834ffc57837adf2f79ed2ad2ad02aa
Ahmed Hasan <ahmed.hasan@mail.utoronto.ca>
2017-10-16 17:37:28 -0400

    Create gc_content.py

---
+++ ldhelmet/gc_content.py
@@ -0,0 +1,37 @@
+'''
+GC_content.py - returns windowed GC content values from fasta files
+
+usage: 
+
+AH - 10/2017
+'''
+
+import argparse
+from Bio import SeqIO
+from tqdm import tqdm
+
+parser = argparse.ArgumentParser(description = 'Get GC content in specified windows from a fasta file.', 
+                                usage = 'gc_content.py [options]')
+parser.add_argument('-i', '--input', required = True,
+                   type = str, help = 'Input fasta. (Required)')
+parser.add_argument('-w', '--windowsize', required = True,
+                   type = int, help = 'Window size. (Required)')
+args = parser.parse_args()
+
+# function(s)
+def GC_content(chrname, seq, windowsize):
+    windows = list(range(0, len(seq), windowsize))
+    windows.append(len(seq))
+    
+    for i in tqdm(range(len(windows) - 1)):
+        subseq = seq[windows[i]:windows[i + 1]]
+        GC = subseq.count('G') + subseq.count('C')
+        out = float(GC) / (windows[i + 1] - windows[i])
+        print(chrname, windows[i], out)
+
+# analysis
+infile = SeqIO.parse(args.input, 'fasta')
+
+print('chr block_start block_GC')
+for record in infile:
+    GC_content(rec.id, rec.seq, args.windowsize)
