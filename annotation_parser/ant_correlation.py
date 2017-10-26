646b421e82ab4779e3a9f0b45bb3e6b745b5f9e0
Ahmed Hasan <ahmed.hasan@mail.utoronto.ca>
2017-10-26 15:38:07 -0400

    reset parser each time

--- annotation_parser/ant_correlation.py
+++ annotation_parser/ant_correlation.py
@@ -41,8 +41,6 @@ del(rholist) # clear up
 
 # introduce annotation table
 
-p = ant.Reader(annotation).fetch(chrom)
-
 def checkrho(record, ld_dict):
     currentrho = 0.0
     for key in ld_dict.keys():
@@ -54,57 +52,68 @@ def checkrho(record, ld_dict):
     return currentrho
 
 print('chrom type avgrho numrecords')
-print('starting exonic')
+
+p = ant.Reader(annotation).fetch(chrom)
 exonic_rho = [checkrho(r, ldh_dict) for r in p if r.is_exonic]
 were_exonic = len(exonic_rho)
 exonic_rho = sum(exonic_rho) / were_exonic # overwrites massive list
 print(chrom, 'exonic', exonic_rho, were_exonic)
 
+p = ant.Reader(annotation).fetch(chrom)
 intronic_rho = [checkrho(r, ldh_dict) for r in p if r.is_intronic]
 were_intronic = len(intronic_rho)
 intronic_rho = sum(intronic_rho) / were_intronic
 print(chrom, 'intronic', intronic_rho, were_intronic)
 
+p = ant.Reader(annotation).fetch(chrom)
 genic_rho = [checkrho(r, ldh_dict) for r in p if r.is_genic]
 were_genic = len(genic_rho)
 genic_rho = sum(genic_rho) / were_genic
 print(chrom, 'genic', genic_rho, were_genic)
 
+p = ant.Reader(annotation).fetch(chrom)
 intergenic_rho = [checkrho(r, ldh_dict) for r in p if r.is_intergenic]
 were_intergenic = len(intergenic_rho)
 intergenic_rho = sum(intergenic_rho) / were_intergenic
 print(chrom, 'intergenic', intergenic_rho, were_intergenic)
 
+p = ant.Reader(annotation).fetch(chrom)
 CDS_rho = [checkrho(r, ldh_dict) for r in p if r.is_in_CDS]
 were_CDS = len(CDS_rho)
 CDS_rho = sum(CDS_rho) / were_CDS
 print(chrom, 'CDS', CDS_rho, were_CDS)
 
+p = ant.Reader(annotation).fetch(chrom)
 fold0_rho = [checkrho(r, ldh_dict) for r in p if r.is_fold0]
 were_fold0 = len(fold0_rho)
 fold0_rho = sum(fold0_rho) / were_fold0
 print(chrom, 'fold0', fold0_rho, were_fold0)
 
+p = ant.Reader(annotation).fetch(chrom)
 fold2_rho = [checkrho(r, ldh_dict) for r in p if r.is_fold2]
 were_fold2 = len(fold2_rho)
 fold2_rho = sum(fold2_rho) / were_fold2
 print(chrom, 'fold2', fold2_rho, were_fold2)
 
+p = ant.Reader(annotation).fetch(chrom)
 fold3_rho = [checkrho(r, ldh_dict) for r in p if r.is_fold3]
 were_fold3 = len(fold3_rho)
 fold3_rho = sum(fold3_rho) / were_fold3
 print(chrom, 'fold3', fold3_rho, were_fold3)
 
+p = ant.Reader(annotation).fetch(chrom)
 fold4_rho = [checkrho(r, ldh_dict) for r in p if r.is_fold4]
 were_fold4 = len(fold4_rho)
 fold4_rho = sum(fold4_rho) / were_fold4
 print(chrom, 'fold4', fold4_rho, were_fold4)
 
+p = ant.Reader(annotation).fetch(chrom)
 utr3_rho = [checkrho(r, ldh_dict) for r in p if r.is_utr3]
 were_utr3 = len(utr3_rho)
 utr3_rho = sum(utr3_rho) / were_utr3
 print(chrom, 'utr3', utr3_rho, were_utr3)
 
+p = ant.Reader(annotation).fetch(chrom)
 utr5_rho = [checkrho(r, ldh_dict) for r in p if r.is_utr5]
 were_utr5 = len(utr5_rho)
 utr5_rho = sum(utr5_rho) / were_utr5
