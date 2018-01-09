f1a58f4a534eb88dc9acd854758854eba6c903c5
Ahmed Hasan <ahmed.hasan@mail.utoronto.ca>
2018-01-09 15:02:11 -0500

    tabs -> spaces

--- ldhelmet/hotspot_annotation.py
+++ ldhelmet/hotspot_annotation.py
@@ -1,9 +1,11 @@
-'''where are hotspots located?
+'''
+where are hotspots located?
 
 script should take in a flat file, select columns of interest, and loop through the annotation table using the boolean attributes
 
 python3.5 hotspot_annotation.py
 
+AH - 01/2018
 '''
 
 import sys
@@ -30,21 +32,20 @@ filename = args.filename
 correlates = dict.fromkeys(['is_genic', 'is_intergenic', 'is_exonic', 'is_intronic', 'is_utr5', 'is_utr3', 'is_in_CDS'], 0)
 
 with open(filename, 'r') as f:
-	for line in tqdm(f):
-		if 'chr,block_start,block_end' in line: # skip header
-			continue
-		else:
-			sp = line.rstrip().split(',')
-			chrom, start, end = sp[1:4]
+    for line in tqdm(f):
+        if 'chr,block_start,block_end' in line: # skip header
+            continue
+        else:
+            sp = line.rstrip().split(',')
+            chrom, start, end = sp[1:4]
 
-			p = antr.Reader(table)
+            p = antr.Reader(table)
 
-			for record in p.fetch(chrom, start, end):
-				for key in correlates:
-					if getattr(record, key):
-						correlates[key] += 1
+            for record in p.fetch(chrom, start, end):
+                for key in correlates:
+                    if getattr(record, key):
+                        correlates[key] += 1
 
 print('correlate count')
 for key in correlates:
-	print(key, correlates[key])
-    
+    print(key, correlates[key])
